e149967f94347bf5a07f0177c7b801a5f1cfb279
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-06-02 23:08:26 +0530

    Add files via upload

---
+++ recursionprintdec.java
@@ -0,0 +1,17 @@
+public class recursionprintdec {
+    public static void printdec(int n){
+    if(n==1)
+    {
+        System.out.println(n);
+        return;
+    }
+    
+        System.out.print(n+" ");
+        printdec(n-1);
+    }
+public static void main(String args[])
+{
+    int n=10;
+    printdec(n);
+}
+}
