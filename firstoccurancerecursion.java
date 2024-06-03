a911947a4222bf226c8df1ed431162ace8024a6d
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-06-03 23:39:03 +0530

    Add files via upload

---
+++ firstoccurancerecursion.java
@@ -0,0 +1,19 @@
+public class firstoccurancerecursion {
+    public static int firstoccurance(int arr[],int key,int i)
+    {
+        if(i==arr.length-1)
+        {
+            return -1;
+        }
+        if(arr[i]==key)
+        {
+            return i;
+        }
+        return firstoccurance(arr, key, i+1);
+    }
+public static void main(String[] args) {
+    int arr[]={8,3,6,9,5,10,2,5,3};
+    System.out.println(firstoccurance(arr,5,0));
+        
+}
+}
