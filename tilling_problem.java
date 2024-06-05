7b166add7bc81bd7c4e58443ad6117957fa76f37
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-06-05 22:55:51 +0530

    Add files via upload

---
+++ tilling_problem.java
@@ -0,0 +1,22 @@
+public class tilling_problem {
+    public static int tillingproblem(int n)
+    {
+        if(n==0||n==1)
+        {
+            return 1;
+        }
+        //kaam
+        //vertical choice
+        int fnm1=tillingproblem(n-1);
+        //horizontal choice
+        
+        int fnm2=tillingproblem(n-2);
+        int totalways=fnm1+fnm2;
+        return totalways;
+    }
+public static void main(String args[])
+{
+    System.out.println(tillingproblem(4));
+
+}
+}
