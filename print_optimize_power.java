34dee48795ba8875427abb98f6e9d602222735fc
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-06-06 23:02:40 +0530

    Add files via upload

---
+++ print_optimize_power.java
@@ -0,0 +1,23 @@
+public class print_optimize_power {
+    public static int optimizedPower(int a,int n)
+    {
+         if(n==0)
+         {
+            return 1;
+         }
+        int halfPowersq=optimizedPower(a,n/2)*optimizedPower(a,n/2);
+        //n is odd
+        if(n%2!=0)
+        {
+            halfPowersq=a*halfPowersq;
+        }
+        return halfPowersq;
+    }
+
+public static void main(String args[])
+{
+    int a=2;
+    int n=10;
+    System.out.println(optimizedPower(a,n));
+}
+}
